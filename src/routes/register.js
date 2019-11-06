74c934c798ec9ae758b33c78e20489fe0eb50544
lazycipher <17141301+lazycipher@users.noreply.github.com>
2019-11-06 06:30:44 +0530

    Email Verification Feature Added

--- src/routes/register.js
+++ src/routes/register.js
@@ -4,7 +4,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const sanitize = require('mongo-sanitize');
-
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const Token = require('../models/token');
 const User = require('../models/user');
 
 // @route    POST api/users
@@ -32,22 +34,25 @@ router.post('/', async (req, res) => {
 
 		await user.save();
 
-		const payload = {
-			user: {
-				uid: user.uid,
-				name: user.name
-			}
+		const token = new Token({ _userId: user.uid, token: crypto.randomBytes(16).toString('hex') });
+		await token.save();
+		var transporter = nodemailer.createTransport({ 
+			service: '"Mailjet"', 
+			auth: { 
+				user: "588003a2b8d598baaa5a03f05fdeeddb", 
+				pass: "d9265a927778dd006c8904d6a127b94c" 
+			} 
+		});
+		var mailOptions = { 
+			from: 'imhim45@outlook.com', 
+			to: user.email, 
+			subject: 'TechnoJam Account Verification', 
+			text: 'Team TechnoJam Welcomes You,\n\n' + `Your Verification Code: ${token.token}\n\nPlease verify your account by clicking the link and submitting the verification code: \nhttp:\/\/` + req.headers.host + '\/api\/verification\/' + token.token + '.\n\n'  + 'Thank You,\nTeam TechnoJam'
 		};
-
-		jwt.sign(
-			payload,
-			config.get('secret'),
-			{ expiresIn: 360000 },
-			(err, token) => {
-				if (err) throw err;
-				res.json({ token });
-			}
-		);
+		transporter.sendMail(mailOptions, (err) => {
+			if (err) { return res.status(500).send({ msg: err.message }); }
+			res.status(201).send('Account Created! Please check your mail to confirm account.');
+		});
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server error');
